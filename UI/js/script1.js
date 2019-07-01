f914976e68dad97aa2522f5adc8830f0c968d49f
Amaobi Victor <aobikobe@gmail.com>
2019-07-01 13:54:11 +0100

    feat(ui): user should be able to create an account - add cors dependency - add spinning loader image - add error span field - add fetch api for creating user
    [Delivers 166982633]

--- UI/js/script1.js
+++ UI/js/script1.js
@@ -1,5 +1,13 @@
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
+const signUpFormEmail = document.querySelector('#email_su');
+const signUpFormFirstName = document.querySelector('#first_name_su');
+const signUpFormLastName = document.querySelector('#last_name_su');
+const signUpFormPassword = document.querySelector('#password_su');
+const signUpFormAddress = document.querySelector('#address_su');
+const signUpErrorSpan = document.querySelector('#sign_up_error');
+const signUpLoader = document.querySelector('#sign_up_loader');
+
 
 const openSignUpPage = () => {
   overlay.classList.add('slide-down');
@@ -9,14 +17,62 @@ const openSignUpPage = () => {
 const closeSignUpPage = () => {
   modal.classList.remove('slide-down');
   overlay.classList.remove('slide-down');
-  if (window.matchMedia('(max-width: 900px)').matches) {
-    // mainDiv.style.display = 'block';
-  }
+  // if (window.matchMedia('(max-width: 900px)').matches) {}
+};
+
+const createUser = (ev) => {
+  signUpLoader.style.display = 'inline-block';
+  const api = 'http://localhost:3000/api/v2/auth/signup';
+  // const api = 'https://quiet-earth-51065.herokuapp.com/api/v2/auth/signup/';
+  const headers = new Headers();
+  headers.set('Content-type', 'application/json');
+  headers.set('Access-Control-Allow-Origin', '*');
+  headers.set('Access-Control-Allow-Headers', 'X-Requested-With');
+
+  // make API call here
+  fetch(api,
+    {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        email: signUpFormEmail.value,
+        firstName: signUpFormFirstName.value,
+        lastName: signUpFormLastName.value,
+        password: signUpFormPassword.value,
+        address: signUpFormAddress.value,
+      }),
+
+    }).then(resp => resp.json()).then((result) => {
+    const { status } = result;
+    if (status === 201) {
+      const { email, token } = result.data;
+
+      localStorage.setItem('email', email);
+      localStorage.setItem('token', token);
+      window.location.replace('./cars.html');
+      return;
+    }
+
+    const { error } = result;
+
+    signUpErrorSpan.textContent = error;
+  })
+    .catch(() => {
+    })
+    .finally(() => {
+      signUpLoader.style.display = 'none';
+    });
+
+
+  ev.preventDefault();
 };
 
 
 const startApp = () => {
   /** Show Sign up form */
+  const signUpForm = document.querySelector('#sign_up_form');
+  signUpErrorSpan.textContent = '';
+  signUpForm.addEventListener('submit', createUser);
 
   const getStartedButton = document.querySelector('button#get-started');
   if (!getStartedButton) return;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import router1 from './routes/route1';
@@ -10,9 +11,11 @@ dotenv.config();
 
 const app = express();
 const portNumber = process.env.PORT || 3000;
-// const router = express.Router();
+process.on('unhandledRejection', () => {
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use('/api/v1', router1);
 app.use('/api/v2', router2);
 
@@ -24,7 +27,7 @@ const options = {
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument, false, options));
 
-function redirectUnmatched(req, res){
+function redirectUnmatched(req, res) {
   return res.status(400).json({ status: 400, error: 'Malformed Path' });
 }
 app.use(redirectUnmatched);
